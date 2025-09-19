3282f4ec2e9c80f62c6ef90781671215a40cbc52
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-09-19 15:25:33 -0500

    push

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import Search from "./components/search";
+
+const API_BASE_URL= 'https://api.themoviedb.org/3'
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+ method:"GET", 
+ headers: {
+  accept: 'application/json',
+  Authorization: `Bearer ${API_KEY}`
+ }
+}
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+
+  }, []);
+
+  return (
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner"/>
+          <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassel</h1>
+        </header>
+check
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <h1 className="text-white">{searchTerm}</h1>
+      </div>
+    </main>
+  )
+}
+
+export default App;
