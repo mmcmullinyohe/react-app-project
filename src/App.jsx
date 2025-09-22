73f53f7b429748356c3d2e2cf47c80911058dd23
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-09-22 12:22:22 -0500

    error response

--- src/App.jsx
+++ src/App.jsx
@@ -16,16 +16,28 @@ const API_OPTIONS = {
 const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
 
+  const [errorMessage, setErrorMessage] = useState("")
+
   const fetchMovies = async () => {
     try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
 
+      const response = await fetch(endpoint, API_OPTIONS);
+      if(!response.ok) {
+        throw new Error ('Failed to fetch movies')
+      }
+
+      const data = await response.json();
+
+      console.log(data);
     } catch (error) {
-      
+      console.error(`Error fetching movies: ${error}`);
+      setErrorMessage('Error fetching movies. Please try again later.');
     }
   }
 
   useEffect(() => {
-
+    fetchMovies();
   }, []);
 
   return (
@@ -35,10 +47,13 @@ const App = () => {
         <header>
           <img src="./hero.png" alt="Hero Banner"/>
           <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassel</h1>
-        </header>
-
         <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-        <h1 className="text-white">{searchTerm}</h1>
+        </header>
+      
+      <section className="all=movies">
+        <h2>All Movies</h2>
+        {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+      </section>
       </div>
     </main>
   )
