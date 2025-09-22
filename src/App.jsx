d71675936df24965891d39193f71967e92f37b4b
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-09-22 13:29:57 -0500

    movie list

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [errorMessage, setErrorMessage] = useState("");
   const [movieList, setMovieList] = useState([]);
-  const [isLoading, setIsLoading] =useState(false);
+  const [isLoading, setIsLoading] =useState(true);
 
   const fetchMovies = async () => {
     setIsLoading(true);
@@ -44,7 +44,7 @@ const App = () => {
       console.error(`Error fetching movies: ${error}`);
       setErrorMessage('Error fetching movies. Please try again later.');
     } finally {
-      setIsLoading(false);
+      setIsLoading(true);
     }
   }
 
@@ -64,7 +64,18 @@ const App = () => {
       
       <section className="all=movies">
         <h2>All Movies</h2>
-        {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+
+        {isLoading ? (
+          <p className="text-white">Loading...</p>
+        ) : errorMessage ? (
+          <p className="text-red-500" >{errorMessage}</p>
+        ) : (
+          <ul>
+            {movieList.map((movie) => (
+              <p key={movie.id} className="text-white">{movie.title}</p>
+            ))}
+          </ul>
+        )}
       </section>
       </div>
     </main>
