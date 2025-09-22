b3ffab8b4f817ec70c9b15c20cbca86e982500f9
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-09-22 13:39:34 -0500

    movie titles

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [errorMessage, setErrorMessage] = useState("");
   const [movieList, setMovieList] = useState([]);
-  const [isLoading, setIsLoading] =useState(true);
+  const [isLoading, setIsLoading] =useState(false);
 
   const fetchMovies = async () => {
     setIsLoading(true);
@@ -45,7 +45,7 @@ const App = () => {
       console.error(`Error fetching movies: ${error}`);
       setErrorMessage('Error fetching movies. Please try again later.');
     } finally {
-      setIsLoading(true);
+      setIsLoading(false);
     }
   }
 
@@ -63,8 +63,8 @@ const App = () => {
         <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
       
-      <section className="all=movies">
-        <h2>All Movies</h2>
+      <section className="all-movies">
+        <h2 className="mt-[40px]">All Movies</h2>
 
         {isLoading ? (
          <Spinner />
