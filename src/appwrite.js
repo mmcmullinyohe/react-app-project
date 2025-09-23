bae3287f6a36cbf6876364c3b98ee87f28bb4880
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-09-23 16:10:11 -0500

    query and ID import

--- src/appwrite.js
+++ src/appwrite.js
@@ -1,29 +1,30 @@
-import { Client, Databases, ID, Query } from "appwrite";
+import { Client, Databases, ID, Query } from 'appwrite'
 
 const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
 const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
 const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
 
 const client = new Client()
-    .setEndpoint('https://nyccloud.appwrite.io/v1')
-    .setProject(PROJECT_ID)
+  .setEndpoint('https://fra.cloud.appwrite.io/v1')
+  .setProject(PROJECT_ID)
 
 const database = new Databases(client);
 
 export const updateSearchCount = async (searchTerm, movie) => {
  
-try {
-const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+ try {
+  const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
     Query.equal('searchTerm', searchTerm),
-])
+  ])
 
+ 
   if(result.documents.length > 0) {
    const doc = result.documents[0];
 
    await database.updateDocument(DATABASE_ID, COLLECTION_ID, doc.$id, {
     count: doc.count + 1,
    })
- 
+
   } else {
    await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {
     searchTerm,
